dd0f894b6a0866d61e8ee0e1a1b751a98db90e86
TahaZahid05 <tz09220@st.habib.edu.pk>
2024-03-31 20:29:43 +0500

    Updated main.py to implenent letter checking

--- main.py
+++ main.py
@@ -9,31 +9,35 @@ def dictionary(filename):
     dict = {}
     for i in range(1,len(data)):
         word = data[i].pop(0)
+        word = word.upper()
         if word in dict:
             dict[word].append(','.join(data[i]))
         else:
             dict[word] = [','.join(data[i])]
+    lst = list()
+    for i in dict:
+        if "-" == i[0] or "'" in i or "/" in i or "." in i or "1" in i or "(" in i or ")" in i:
+            lst.append(i)
+    for i in range(len(lst)):
+        del dict[lst[i]]
     return dict
 
 dictionary = dictionary('english.csv')
-print(dictionary)
 
 def wordDict(dictionary):
     returnDict = dict()
-    upperletterHolder = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    specialCase = ["-","'"]
-    for i in dictionary:
-        if i in upperletterHolder:
-            if i not in returnDict:
-                returnDict[i] = list()
-                returnDict[i.lower()] = list()
-    for i in specialCase:
+    upperletterHolder = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'," ","-"]
+    for i in upperletterHolder:
         returnDict[i] = list()
-    print(returnDict)
-    # for j in dictionary:
-    #     if j not in upperletterHolder:
-    #         print(j)
-    #         returnDict[j[0]].append(j[1])
-    # return returnDict
+    lst = list()
+    for i in dictionary:
+        lst.append(i)
+    # print(lst)
+    for i in range(len(lst)):
+        print(lst[i])
+        for j in range(1,len(lst[i])):
+            if lst[i][j] not in returnDict[lst[i][j-1]]:
+                returnDict[lst[i][j-1]].append(lst[i][j])
+    return returnDict
 
 print(wordDict(dictionary))
