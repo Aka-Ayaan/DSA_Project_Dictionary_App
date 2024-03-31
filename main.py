e41fef46c669c0c606baece3d8a8ccbb0cc8ba06
ramshaarshad04 <ra09278@st.habib.edu.pk>
2024-03-31 15:11:26 +0500

    hello

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def lol(argument):
-    print("yess")
+def dictionary(filename):
+    with open(filename) as f:
+        data = f.readlines()
+    for i in range(len(data)):
+        data[i] = data[i].strip().split(',')
+    dict = {}
+    for i in range(len(data)):
+        word = data[i].pop(0)
+        if word in dict:
+            dict[word].append(','.join(data[i]))
+        else:
+            dict[word] = [','.join(data[i])]
+    print(dict)
 
-    print("lalala")
+dictionary('english.csv')
