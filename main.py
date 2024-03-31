81bdc4534a8049585b990f66f188c039aff846df
TahaZahid05 <tz09220@st.habib.edu.pk>
2024-03-31 17:33:41 +0500

    commeet

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ def dictionary(filename):
     with open(filename) as f:
         data = f.readlines()
     for i in range(len(data)):
-        if i >= 127358 and i <= 139845:
+        if i >= 127357 and i <= 139844:
             data[i] = data[i].strip().split(' ')
         else:
             data[i] = data[i].strip().split(',')
-        
-
     dict = {}
-    for i in range(len(data)):
+    for i in range(1,len(data)):
         word = data[i].pop(0)
         if word in dict:
             dict[word].append(','.join(data[i]))
         else:
             dict[word] = [','.join(data[i])]
-    print(dict['Rytina'])
+    return dict
 
-dictionary('english.csv')
+dictionary = dictionary('english.csv')
+
+def wordDict(dictionary):
+    returnDict = dict()
+    upperletterHolder = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    specialCase = ["-","'"]
+    for i in dictionary:
+        if i in upperletterHolder:
+            if i not in returnDict:
+                returnDict[i] = list()
+                returnDict[i.lower()] = list()
+    for i in specialCase:
+        returnDict[i] = list()
+    print(returnDict)
+    # for j in dictionary:
+    #     if j not in upperletterHolder:
+    #         print(j)
+    #         returnDict[j[0]].append(j[1])
+    # return returnDict
+
+print(wordDict(dictionary))
