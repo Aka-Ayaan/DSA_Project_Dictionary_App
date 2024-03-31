71f21b3a5a4ae9b3f06167bfe6dbd52fb5f4b6b1
ramshaarshad04 <ra09278@st.habib.edu.pk>
2024-03-31 16:47:41 +0500

    sksksk

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def dictionary(filename):
     with open(filename) as f:
         data = f.readlines()
     for i in range(len(data)):
-        data[i] = data[i].strip().split(',')
+        if i >= 127358 and i <= 139845:
+            data[i] = data[i].strip().split(' ')
+        else:
+            data[i] = data[i].strip().split(',')
+        
+
     dict = {}
     for i in range(len(data)):
         word = data[i].pop(0)
@@ -10,6 +15,6 @@ def dictionary(filename):
             dict[word].append(','.join(data[i]))
         else:
             dict[word] = [','.join(data[i])]
-    print(dict)
+    print(dict['Rytina'])
 
 dictionary('english.csv')
