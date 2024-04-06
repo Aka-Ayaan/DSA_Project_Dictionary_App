c7dd7964dbb069665d288ff39626950a9cb6b678
TahaZahid05 <tz09220@st.habib.edu.pk>
2024-04-06 15:17:11 +0500

    Revert "Added dictionary changes and admin user interface"
    This reverts commit 828df7004e6f7364e2f99b93d3c1095fd2619964.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import csv
-import pandas as pd
 #CSV to dictionary word and meaning
-def dictionaryCreate(filename):
+def dictionary(filename):
     with open(filename) as f:
         data = f.readlines()
     for i in range(len(data)):
-        if i >= 127339 and i <= 139825:
+        if i >= 127357 and i <= 139844:
             data[i] = data[i].strip().split(' ')
         else:
-            #User-defined words from 188510
             data[i] = data[i].strip().split(',')
     dict = {}
     for i in range(1,len(data)):
@@ -40,17 +37,16 @@ def make_trie(dictionary):
 
 
 #get function
-def in_trie(trie, word, dictionary, returnAfterInsert = False):
+def in_trie(trie, word, returnAfterInsert = False):
     temp = word.upper()
     current_dict = trie
     for letter in temp:
         if letter not in current_dict:
             takeChoice = input("No such word in dictionary. Want to insert a new word?")
             if takeChoice.upper() == "YES":
-                verb = input("What part of speech is it? (Verb,Noun,adjective,preposition,etc) Can skip if don't know: ")
                 meaning = input("Enter your meaning: ")
-                trie = insert_trie(trie,word,meaning,verb,dictionary)
-                return in_trie(trie,word,dictionary,True)
+                trie = insert_trie(trie,word,meaning)
+                return in_trie(trie,word,True)
             else:
                 return "Exiting..."
         current_dict = current_dict[letter]
@@ -70,9 +66,8 @@ def in_trie(trie, word, dictionary, returnAfterInsert = False):
                     return "Great! Exiting program.."
                 elif choice.upper() == "NO":
                     print("Add your own meaning to this word:")
-                    verb = input("What part of speech is it? (Verb,Noun,adjective,preposition,etc) Can skip if don't know: ")
                     meaning = input("Enter your meaning: ")
-                    trie = insert_trie(trie,word,meaning,verb,dictionary)
+                    trie = insert_trie(trie,word,meaning)
                     str1 = word + ": " + str(current_dict["_end"])
                     return str1
                 else:
@@ -81,26 +76,15 @@ def in_trie(trie, word, dictionary, returnAfterInsert = False):
     else:
         takeChoice = input("No such word in dictionary. Want to insert a new word?")
         if takeChoice.upper() == "YES":
-            verb = input("What part of speech is it? (Verb,Noun,adjective,preposition,etc) Can skip if don't know: ")
             meaning = input("Enter your meaning: ")
-            trie = insert_trie(trie,word,meaning,verb,dictionary)
-            str1 = in_trie(trie,word,dictionary,True)
+            trie = insert_trie(trie,word,meaning)
+            str1 = in_trie(trie,word,True)
             return str1
         else:
             return "Exiting..."
 
-#Adds the words user has entered into the csv file in order to save them permenatnely (even for the next time the program runs)
-def writeToCSV(word,verb,meaning,trie,dictionary):
-    verb = verb + "."
-    with open("english.csv","a",newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([word,verb,meaning])
-    dictionary = dictionaryCreate("english.csv")
-    trie = make_trie(dictionary)
-    return (trie,dictionary)
-
 #Insert function
-def insert_trie(trie,word,meaning,verb,dictionary):
+def insert_trie(trie,word,meaning):
     temp = word.upper()
     current_dict = trie
     flag = True
@@ -109,22 +93,14 @@ def insert_trie(trie,word,meaning,verb,dictionary):
             flag = False
             break
         current_dict = current_dict[letter]
-    toBeAppended = verb + ".," + meaning
     if flag:
         if "_end" in current_dict:
             print("Word already exists")
-            if toBeAppended not in current_dict["_end"]:
-                current_dict["_end"].append(toBeAppended)
-                temp = writeToCSV(word,verb,meaning,trie,dictionary)
-                trie , dictionary = temp[0], temp[1]
-                print("Meaning added succesfully")
-                return trie
-            else:
-                return "Meaning already in dictionary!"
+            current_dict["_end"].append(meaning)
+            print("Meaning added succesfully")
+            return trie
         else:
-            current_dict["_end"] = [toBeAppended]
-            temp = writeToCSV(word,verb,meaning,trie,dictionary)
-            trie , dictionary = temp[0], temp[1]
+            current_dict["_end"] = [meaning]
             print("Word entered succesfully")
             return trie
     else:
@@ -133,42 +109,11 @@ def insert_trie(trie,word,meaning,verb,dictionary):
             if letter not in current_dict:
                 current_dict[letter] = {}
             current_dict = current_dict[letter]
-        current_dict["_end"] = [toBeAppended]
-        temp = writeToCSV(word,verb,meaning,trie,dictionary)
-        trie , dictionary = temp[0], temp[1]
+        current_dict["_end"] = [meaning]
         print("Word entered succesfully")
         return trie
     
-def delete_word_from_CS(word,trie,dictionary):
-    with open("english.csv","r") as f:
-        lines = f.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].strip()
-        if i >= 127339 and i <= 139825:
-            lines[i] = lines[i].strip().split(' ')
-        else:
-            lines[i] = lines[i].strip().split(',')
-    with open("english.csv","w",newline='') as f:
-        csvwriter = csv.writer(f)
-        countX = 127339
-        countY = 139825
-        for i in range(len(lines)):
-            if word != lines[i][0]:
-                if i >= countX and i <= countY:
-                    data = lines[i][0] + " (" + lines[i][1] + ") " + lines[i][2]
-                    csvwriter.writerow([data])
-                else:
-                    # print(lines[i])
-                    lines[i][2] = lines[i][2].strip('""')
-                    csvwriter.writerow([lines[i][0],lines[i][1],lines[i][2]])
-            else:
-                countX -= 1
-                countY -= 1
-    dictionary = dictionaryCreate("english.csv")
-    trie = make_trie(dictionary)
-    return (trie,dictionary)
-
-def delete_trie_word(trie,word,dicitionary):
+def delete_trie_word(trie,word):
     temp = word.upper()
     current_dict = trie
     for letter in temp:
@@ -176,8 +121,8 @@ def delete_trie_word(trie,word,dicitionary):
             takeChoice = input("No such word in dictionary. Want to insert a new word?")
             if takeChoice.upper() == "YES":
                 meaning = input("Enter your meaning: ")
-                trie = insert_trie(trie,word,meaning,dictionary)
-                return in_trie(trie,word,dictionary,True)
+                trie = insert_trie(trie,word,meaning)
+                return in_trie(trie,word,True)
             else:
                 return "Exiting..."
         current_dict = current_dict[letter]
@@ -185,74 +130,25 @@ def delete_trie_word(trie,word,dicitionary):
         takeChoice = input("No such word in dictionary. Want to insert a new word?")
         if takeChoice.upper() == "YES":
             meaning = input("Enter your meaning: ")
-            trie = insert_trie(trie,word,meaning,dictionary)
-            str1 = in_trie(trie,word,dictionary,True)
+            trie = insert_trie(trie,word,meaning)
+            str1 = in_trie(trie,word,True)
             return str1
         else:
             return "Exiting..."
     else:
         del current_dict["_end"]
-        delete_word_from_CS(word,trie,dictionary)
         return "Word removed succesfully"
 
-def reset(dictionary,trie):
-    with open("original.csv","r") as f:
-        lines = f.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].strip()
-        if i >= 127339 and i <= 139825:
-            lines[i] = lines[i].strip().split(' ')
-        else:
-            lines[i] = lines[i].strip().split(',')
-    with open("english.csv","w",newline='') as f:
-        csvwriter = csv.writer(f)
-        for i in range(len(lines)):
-            if i >= 127339 and i <= 139825:
-                data = lines[i][0] + " (" + lines[i][1] + ") " + lines[i][2]
-                csvwriter.writerow([data])
-            else:
-                # print(lines[i])
-                lines[i][2] = lines[i][2].strip('""')
-                csvwriter.writerow([lines[i][0],lines[i][1],lines[i][2]])
-    dictionary = dictionaryCreate("english.csv")
-    trie = make_trie(dictionary)
-    return (trie,dictionary)
-    
-def delete_meaning_from_CSV(word,verb,meaning,dictionary,trie):
-    verb = verb + "."
-    with open("english.csv","r") as f:
-        lines = f.readlines()
-    for i in range(len(lines)):
-        lines[i] = lines[i].strip()
-        if i >= 127339 and i <= 139825:
-            lines[i] = lines[i].strip().split(' ')
-        else:
-            lines[i] = lines[i].strip().split(',')
-    with open("english.csv","w",newline='') as f:
-        csvwriter = csv.writer(f)
-        for i in range(len(lines)):
-            if word not in lines[i] or verb not in lines[i] or meaning not in lines[i]:
-                if i >= 127339 and i <= 139825:
-                    data = lines[i][0] + " (" + lines[i][1] + ") " + lines[i][2]
-                    csvwriter.writerow([data])
-                else:
-                    lines[i][2] = lines[i][2].strip('""')
-                    csvwriter.writerow([lines[i][0],lines[i][1],lines[i][2]])
-    dictionary = dictionaryCreate("english.csv")
-    trie = make_trie(dictionary)
-    return (trie,dictionary)
-
-def delete_trie_word_meaning(trie,word,meaning,verb,dictionary):
+def delete_trie_word_meaning(trie,word,meaning):
     temp = word.upper()
     current_dict = trie
     for letter in temp:
         if letter not in current_dict:
-            print("god")
             takeChoice = input("No such word in dictionary. Want to insert a new word?")
             if takeChoice.upper() == "YES":
                 meaning = input("Enter your meaning: ")
-                trie = insert_trie(trie,word,meaning,dictionary)
-                return in_trie(trie,word,dictionary,True)
+                trie = insert_trie(trie,word,meaning)
+                return in_trie(trie,word,True)
             else:
                 return "Exiting..."
         current_dict = current_dict[letter]
@@ -260,8 +156,8 @@ def delete_trie_word_meaning(trie,word,meaning,verb,dictionary):
         takeChoice = input("No such word in dictionary. Want to insert a new word?")
         if takeChoice.upper() == "YES":
             meaning = input("Enter your meaning: ")
-            trie = insert_trie(trie,word,meaning,dictionary)
-            str1 = in_trie(trie,word,dictionary,True)
+            trie = insert_trie(trie,word,meaning)
+            str1 = in_trie(trie,word,True)
             return str1
         else:
             return "Exiting..."
@@ -269,15 +165,11 @@ def delete_trie_word_meaning(trie,word,meaning,verb,dictionary):
         if len(current_dict["_end"]) == 1:
             flagInput = False
             while not flagInput:
-                print(word,"only contains the one meaning. Deleting this meaning would also delete the word as there are no other meanings!. Continue?")
+                print(word,"only contains the meaning you have asked to delete. Deleting this meaning would also delete the word as there are no other meanings!. Continue?")
                 userInput = input()
                 if userInput.upper() == "YES":
-                    print(meaning)
-                    print(current_dict["_end"])
-                    if meaning in current_dict["_end"][0]:
+                    if meaning in current_dict["_end"]:
                         del current_dict["_end"]
-                        temp = delete_meaning_from_CSV(word,verb,meaning,dictionary,trie)
-                        trie, dictionary = temp[0], temp[1]
                         return "Meaning and word successfully deleted."
                     else:
                         return "No such meaning exists."
@@ -287,103 +179,56 @@ def delete_trie_word_meaning(trie,word,meaning,verb,dictionary):
                     print("Invalid input. Answer with either yes or no")
         else:
             for i in range(len(current_dict["_end"])):
-                if meaning in current_dict["_end"][i]:
+                if current_dict["_end"][i] == meaning:
                     del current_dict["_end"][i]
-                    temp = delete_meaning_from_CSV(word,verb,meaning,dictionary,trie)
-                    trie, dictionary = temp[0], temp[1]
                     print(word + ": " + str(current_dict["_end"]))
                     return "Meaning successfully deleted."
             return "No such meaning exists."
 
 
-dictionary = dictionaryCreate('english.csv')
+dictionary = dictionary('english.csv')
 trie = make_trie(dictionary)
 flag = True
 while flag:
-    logInFlag = False
-    while not logInFlag:
-        print(logInFlag)
-        initalInput = int(input("Select interface:\n1)Admin\n2)User\n"))
-        if initalInput == 1:
-            passFlag = False
-            while not passFlag:
-                userInput = input("Enter password: ")
-                passStore = "hehe"
-                if userInput == passStore:
-                    passFlag = True
-                    operationFlag = False
-                    while not operationFlag:
-                        userSelect = int(input("Select operation:\n1)Reset dictionary\n"))
-                        if userSelect == 1:
-                            temp = reset(dictionary,trie)
-                            trie, dictionary = temp[0], temp[1]
-                            operationFlag = True
-                            print("Dictionary resetted!")
-                        else:
-                            print("Invalid input. Enter 1 to continue")
-                else:
-                    print("Wrong password")
-                    choice = input("Select 1 or 2:\n1)Try again\n2)Go to user interface\n") 
-                    if choice == 2:
-                        initalInput = 2
-            flagInput = False
-            while not flagInput:
-                choice = input("Exit the program?: ")
-                if choice.upper() == "YES":
-                    logInFlag = True
-                    flagInput = True
-                    flag = False
-                elif choice.upper() == "NO":
-                    flagInput = True
-                else:
-                    print("Invalid input. Answer with either yes or no")
-        elif initalInput == 2:   
-            mainFlag = False
-            while not mainFlag:
-                userInput = int(input("Select your operation by entering the number:\n1)Insert\n2)Get\n3)Delete\n4)Exit\n"))
-                if userInput == 1:
+    mainFlag = False
+    while not mainFlag:
+        userInput = int(input("Select your operation by entering the number:\n1)Insert\n2)Get\n3)Delete\n"))
+        if userInput == 1:
+            word = input("Enter your word: ")
+            meaning = input("Enter your meaning: ")
+            trie = insert_trie(trie,word,meaning)
+            print(in_trie(trie,word, True))
+            mainFlag = True
+        elif userInput == 2:
+            word = input("Enter your word: ")
+            print(in_trie(trie,word))
+            mainFlag = True
+        elif userInput == 3:
+            select = int(input("Choose your operation:\n1)Delete the word\n2)Delete a meaning of the word\n"))
+            flagCheck = False
+            while not flagCheck:
+                if select == 1:
                     word = input("Enter your word: ")
-                    verb = input("What part of speech is it? (Verb,Noun,adjective,preposition,etc). Can skip if don't know: ")
+                    print(delete_trie_word(trie,word))
+                    flagCheck = True
+                    mainFlag = True
+                elif select == 2:
+                    word = input("Enter your word: ")
                     meaning = input("Enter your meaning: ")
-                    temp = insert_trie(trie,word,meaning,verb,dictionary)
-                    if type(temp) != str:
-                        trie = temp
-                        print(in_trie(trie,word,dictionary,True))
-                    else:
-                        print(temp)
-                elif userInput == 2:
-                    word = input("Enter your word: ")
-                    print(in_trie(trie,word,dictionary))
-                elif userInput == 3:
-                    select = int(input("Choose your operation:\n1)Delete the word\n2)Delete a meaning of the word\n"))
-                    flagCheck = False
-                    while not flagCheck:
-                        if select == 1:
-                            word = input("Enter your word: ")
-                            print(delete_trie_word(trie,word,dictionary))
-                            flagCheck = True
-                        elif select == 2:
-                            word = input("Enter your word: ")
-                            verb = input("What part of speech is it? (Verb,Noun,adjective,preposition,etc). Can skip if don't know: ")
-                            meaning = input("Enter your meaning: ")
-                            print(delete_trie_word_meaning(trie,word,meaning,verb,dictionary))
-                            flagCheck = True
-                        else:
-                            print("Invalid input. Answer with 1 or 2")
-                elif userInput == 4:
+                    print(delete_trie_word_meaning(trie,word,meaning))
+                    flagCheck = True
                     mainFlag = True
                 else:
-                    print("Invalid input. Enter a number between 1 and 3.")
-                    continue
-            flagInput = False
-            while not flagInput:
-                choice = input("Back to interface selection: ")
-                if choice.upper() == "NO":
-                    print("Exiting program")
-                    flagInput = True
-                    logInFlag = True
-                    flag = False
-                else:
-                    print("Invalid input. Answer with either yes or no")
+                    print("Invalid input. Answer with 1 or 2")
         else:
-            print("Invalid input. Enter 1 or 2 to continue")
+            print("Invalid input. Enter a number between 1 and 3.")
+    flagInput = False
+    while not flagInput:
+        choice = input("Exit the program?: ")
+        if choice.upper() == "YES":
+            flagInput = True
+            flag = False
+        elif choice.upper() == "NO":
+            flagInput = True
+        else:
+            print("Invalid input. Answer with either yes or no")
